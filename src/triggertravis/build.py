dfac4d9eeeb5ca13313abbc76e4985caa773dd69
Ian Maddison <ian@i.need.stronger.coffee>
2015-04-21 11:54:08 +0100

    Add slack notifications

--- src/triggertravis/build.py
+++ src/triggertravis/build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, json
+import os, json, requests
 
 from github import GitHub
 from heroku import Heroku
@@ -14,6 +14,21 @@ travis = Travis(os.getenv('TRAVIS_ENDPOINT', 'https://api.travis-ci.org/'), os.g
 APPS = json.loads(os.getenv('APPS'))
 E2E_OWNER = os.getenv('E2E_OWNER')
 E2E_REPO = os.getenv('E2E_REPO')
+SLACK_URL = os.getenv('SLACK_URL')
+SLACK_CHANNEL = os.getenv('SLACK_CHANNEL')
+
+
+
+def notify(message, emoji = ":monkey_face:"):
+    if SLACK_URL and SLACK_CHANNEL:
+        data = {
+            "icon_emoji": emoji,
+            "channel": SLACK_CHANNEL,
+            "username": "buildbot",
+            "text": message
+        }
+    
+        requests.post(SLACK_URL, data = json.dumps(data))
 
 
 def deploy_and_test(repo_owner, repo_name, branch, commit_id):
@@ -21,7 +36,7 @@ def deploy_and_test(repo_owner, repo_name, branch, commit_id):
     if repo_path in APPS:
         app_name = APPS[repo_path]
         url = github.get_source_tarball(repo_owner, repo_name, commit_id)
-        print "Deploying {}/{} at {} to Heroku app {} from {}".format(repo_owner, repo_name, commit_id, app_name, url)
+        notify("{}: {} merged to master, deploying to {}".format(repo_name, commit_id, app_name))
         (deployed, message) = heroku.deploy_and_wait(app_name, url, commit_id)
         if deployed:
             print "Deployed"
@@ -49,7 +64,8 @@ def deploy_and_test(repo_owner, repo_name, branch, commit_id):
                             break
 
                 time.sleep(5)
-    
+                
+            notify("{}: running end to end tests for {}".format(repo_name, app_name))
             print "Started build {}".format(running_build['id'])
         else:
             print "Deploy failed: {}".format(message)
